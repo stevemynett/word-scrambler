199f18577a24605dab1b95280e45ec5b4ba19ca1
Steve Mynett <steve@mynettworks.com>
2014-08-31 20:55:06 -0700

    Changing spaces to tabs

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,48 @@
 $(function() {
 
 
-	/*
-		Shuffles the letters in a word
-		http://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
-		@usage console.log("the quick brown fox jumps over the lazy dog".shuffle());
-	*/
-	String.prototype.shuffle = function () {
-	    var a = this.split(""),
-		    n = a.length;
+  /*
+    Shuffles the letters in a word
+    http://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
+    @usage console.log("the quick brown fox jumps over the lazy dog".shuffle());
+  */
+  String.prototype.shuffle = function () {
+      var a = this.split(""),
+        n = a.length;
 
-	    for(var i = n - 1; i > 0; i--) {
-	        var j = Math.floor(Math.random() * (i + 1));
-	        var tmp = a[i];
-	        a[i] = a[j];
-	        a[j] = tmp;
-	    }
-	    return a.join("");
-	}
+      for(var i = n - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var tmp = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+      }
+      return a.join("");
+  }
 
-	// Lets get a list of long words
-	var wordList = ['Allegator', 'Blunderbuss', 'Brouhaha', 'Bumbershoot', 'Cantankerous', 'Cockamamie', 'Crapulence', 'Donnybrook', 'Doozy', 'Dudgeon', 'Filibuster', 'Formication', 'Gastromancy', 'Kerfuffle', 'Klutz', 'Malarkey', 'Maverick', 'Nincompoop', 'Ornery', 'Panjandrum', 'Pettifogger', 'Pratfall', 'Rambunctious', 'Shenanigan', 'Skedaddle', 'Troglodyte', 'Vomitory', 'Wabbit', 'Yahoo'];
-	
-	// randomly generate the word
-	var word = wordList[Math.floor(Math.random()*wordList.length)];
-	var scrambledWord = word.shuffle().toLowerCase();
-	$('.scrambledNewWord').append(scrambledWord);
+  // Lets get a list of long words
+  var wordList = ['Allegator', 'Blunderbuss', 'Brouhaha', 'Bumbershoot', 'Cantankerous', 'Cockamamie', 'Crapulence', 'Donnybrook', 'Doozy', 'Dudgeon', 'Filibuster', 'Formication', 'Gastromancy', 'Kerfuffle', 'Klutz', 'Malarkey', 'Maverick', 'Nincompoop', 'Ornery', 'Panjandrum', 'Pettifogger', 'Pratfall', 'Rambunctious', 'Shenanigan', 'Skedaddle', 'Troglodyte', 'Vomitory', 'Wabbit', 'Yahoo'];
 
-	// read input from dom on submit
-	$('#submit').on('click', function() {
-		var submittedWord = $('.answer').val();
-		var body = $('body');
+  // randomly generate the word
+  var word = wordList[Math.floor(Math.random()*wordList.length)];
+  var scrambledWord = word.shuffle().toLowerCase();
+  $('.scrambledNewWord').append(scrambledWord);
 
-		// evaluate the word
-		if (word.toLowerCase() == submittedWord.toLowerCase()) {
-			body.addClass('correct');
-			body.removeClass('incorrect');
-		} else { 
-			body.addClass('incorrect');
-			body.removeClass('correct');
-		}
-	})
+  // read input from dom on submit
+  $('#submit').on('click', function() {
+    var submittedWord = $('.answer').val();
+    var body = $('body');
 
-	// refresh the word
+    // evaluate the word
+    if (word.toLowerCase() == submittedWord.toLowerCase()) {
+      body.addClass('correct');
+      body.removeClass('incorrect');
+    } else {
+      body.addClass('incorrect');
+      body.removeClass('correct');
+    }
+  })
 
-	// $('#reset').on('click', generateWord());
+  // refresh the word
+
+  // $('#reset').on('click', generateWord());
 });
